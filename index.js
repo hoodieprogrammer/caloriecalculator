a7b21cd41d5c2bc1786bcba793a04afbd2639729
hoodieprogrammer <159121112+hoodieprogrammer@users.noreply.github.com>
2024-03-25 09:57:20 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById("submit")
+const select = document.getElementById("selector")
+const weightbox = document.getElementById("weightbox")
+const heightbox = document.getElementById("heightbox")
+
+function onClick() {
+   
+}
+
+btn.addEventListener("click", () => onClick())
+
